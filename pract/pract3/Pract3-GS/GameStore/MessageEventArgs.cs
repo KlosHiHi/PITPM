93d75f55b7fdfb708cb198a463b8ac54247915a6
221 <221@PC-Klass.ru>
2025-09-25 11:59:36 +0300

    pract3

---
+++ pract/pract3/Pract3-GS/GameStore/MessageEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace GameStore
+{
+    public class MessageEventArgs : EventArgs
+    {
+        public MessageEventArgs(string message, string caption, MessageBoxButton button, MessageBoxImage icon)
+        {
+            Message = message;
+            Caption = caption;
+            Button = button;
+            Icon = icon;
+        }
+
+        public string Message { get; set; }
+        public string Caption { get; set; }
+        public MessageBoxButton Button { get; set; }
+        public MessageBoxImage Icon { get; set; }
+    }
+}
---
+++ pract/pract3/Pract3-GS/GameStore/Program.cs
@@ -0,0 +1,21 @@
+﻿using GameStore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+public class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        var host = Host.CreateDefaultBuilder()
+            .ConfigureServices(services =>
+            {
+                services.AddSingleton<App>();
+                services.AddSingleton<MainWindow>();
+
+            })
+            .Build();
+        var app = host.Services.GetService<App>();
+        app?.Run();
+    }
+}
