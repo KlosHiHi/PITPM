96c104ffd2f731533dc57bdcc57e036572c5a983
221 <221@PC-Klass.ru>
2025-10-01 11:55:37 +0300

    0110lec

--- lections/09262025/UnitTests/UnitTest1.cs
+++ lections/09262025/UnitTests/UnitTest1.cs
@@ -1,6 +1,8 @@
 using FlaUI.Core;
 using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Definitions;
 using FlaUI.UIA3;
+using FluentAssertions;
 
 namespace UnitTests
 {
@@ -9,31 +11,46 @@ namespace UnitTests
         public UIA3Automation Automation { get; }
         public Application Application { get; }
 
-        public UITestsFixture() 
-        { 
+        public UITestsFixture()
+        {
             Automation = new UIA3Automation();
-            Application = Application.Launch(Path.Combine(Environment.CurrentDirectory, "TestApp.exe"));
+            Application = Application.Launch(Path.Combine("C:\\Temp\\.ispp31\\course3\\PITPM\\lections\\09262025\\TestApp\\bin\\Debug\\net8.0-windows\\TestApp.exe"));
         }
 
-        public void Dispose() 
-        { 
+        public void Dispose()
+        {
+            Application.Close();
             Automation.Dispose();
             Application.Dispose();
         }
-    } 
+    }
 
-    public class UnitTest1(UITestsFixture fixture) :IClassFixture<UITestsFixture>
+    public class UnitTest1(UITestsFixture fixture) : IClassFixture<UITestsFixture>
     {
         private UITestsFixture fixture = fixture;
 
         [Fact]
-        public void Test1()
+        public async Task Test1Async()
         {
             var window = fixture.Application.GetMainWindow(fixture.Automation);
 
-            var button = window.FindFirstDescendant(c=> c.ByText("ÆÌÈ!")).AsButton;
+            var button = window.FindFirstDescendant(c => c.ByText("ÆÌÈ!")).AsButton();
 
             button.Invoke();
+
+            var input = window.FindFirstDescendant(c => c.ByAutomationId("InputTextBox")).AsTextBox();
+
+            input.Enter("éöóêåí");
+
+            var label = window.FindFirstDescendant(c => c.ByAutomationId("CountTextBlock")).AsLabel();
+
+            await Task.Delay(250);
+            window.CaptureToFile("screen1.png");
+            await Task.Delay(250);
+            label.Text.Should().Be("1");
+
+            var label1 = window.FindFirstDescendant(c => c.ByAutomationId("CountTextBlock")
+                                            .Or(c.ByControlType(ControlType.Text))).AsLabel();
         }
     }
 }
