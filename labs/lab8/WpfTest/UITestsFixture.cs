f50bedb049dba54319516597748177e817a74778
KlosHiHi <vaceslavvlasov420@gmail.com>
2025-10-09 19:00:25 +0300

    lab8Fin

--- labs/lab8/WpfTest/UITestsFixture.cs
+++ labs/lab8/WpfTest/UITestsFixture.cs
@@ -11,8 +11,8 @@ namespace WpfTest
         public UITestsFixture()
         {
             Automation = new UIA3Automation();
-            //Application = Application.Launch("C:\\lab8\\Wpf\\bin\\Debug\\net8.0-windows\\SimpleWpf.exe");
-            Application = Application.Launch("C:\\Temp\\.ispp31\\course3\\PITPM\\labs\\lab8\\Wpf\\bin\\Debug\\net8.0-windows\\SimpleWpf.exe");
+            Application = Application.Launch("C:\\Users\\Вячеслав\\Desktop\\5\\programming\\COURSE3\\PITPM\\labs\\lab8\\Wpf\\bin\\Debug\\net8.0-windows\\SimpleWpf.exe");
+            //Application = Application.Launch("C:\\Temp\\.ispp31\\course3\\PITPM\\labs\\lab8\\Wpf\\bin\\Debug\\net8.0-windows\\SimpleWpf.exe");
         }
 
         public void Dispose()
