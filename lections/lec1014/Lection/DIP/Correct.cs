fbf6de9946205232a980e26accfbe919ef589345
221 <221@PC-Klass.ru>
2025-10-14 10:10:52 +0300

    lec1014

---
+++ lections/lec1014/Lection/DIP/Correct.cs
@@ -0,0 +1,31 @@
+﻿using Lection.DIP;
+
+namespace Lection.DIP
+{
+    public interface IPaymentProcessor { void Charge(decimal amount); }
+
+    public class StripePaymentProcessor1 : IPaymentProcessor
+    {
+        public void Charge(decimal amount)
+        {
+            //Do Something
+        }
+    }
+}
+
+public class OrderProcessor1
+{
+    private readonly IPaymentProcessor _processor;
+
+    public OrderProcessor1(IPaymentProcessor processor)
+    {
+        _processor = processor;
+    }
+
+    public void Process(Order order)
+    {
+        var payment = new StripePaymentProcessor1();
+        payment.Charge(order.Amount);
+    }
+}
+}
